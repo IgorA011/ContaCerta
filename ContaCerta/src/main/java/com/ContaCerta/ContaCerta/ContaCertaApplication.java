add769900e3ce7b34640e47472501e36189d27fa
Igor Alves <igor_alvesoliveira@hotmail.com.br>
2025-08-31 16:50:30 -0300

    Primeiro commit - Projeto ContaCerta

---
+++ ContaCerta/src/main/java/com/ContaCerta/ContaCerta/ContaCertaApplication.java
@@ -0,0 +1,13 @@
+package com.ContaCerta.ContaCerta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContaCertaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContaCertaApplication.class, args);
+	}
+
+}
---
+++ ContaCerta/src/test/java/com/ContaCerta/ContaCerta/ContaCertaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ContaCerta.ContaCerta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContaCertaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
