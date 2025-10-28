75ce098f6fc5892ffd8c37401b406954f990c585
IgorA011 <igor_alvesoliveira@hotmail.com.br>
2025-10-27 23:44:43 -0300

    teste

--- src/main/java/com/contacerta/ContacertaApplication.java
+++ src/main/java/com/contacerta/ContacertaApplication.java
@@ -1,13 +1,13 @@
-// FinanceiroApplication.java
+// ContacertaApplication.java
 package com.contacerta;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FinanceiroApplication {
+public class ContacertaApplication {
     public static void main(String[] args) {
-        SpringApplication.run(FinanceiroApplication.class, args);
+        SpringApplication.run(ContacertaApplication.class, args);
     }
 }
 
